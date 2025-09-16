64ee008eb4c384f9f639ba997b0726c9d5f234dc
axonde <alex.chpak@gmail.com>
2025-09-16 21:01:01 +0300

    try to fix deploy styles linking problem

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 
-const dev = process.argv.includes('dev');
-
 const config = {
   kit: {
     adapter: adapter(),
     paths: {
-      base: dev ? '' : '/alex-nevsky',
+      base: '/alex-nevsky'
     }
   }
 };
 
 export default config;
-
