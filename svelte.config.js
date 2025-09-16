eaec2906999c7f82381c0e2ba89940c38f1a0ca2
axonde <alex.chpak@gmail.com>
2025-09-16 21:10:55 +0300

    [try to solve gh pages]

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,16 @@
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.argv.includes('dev');
+
 const config = {
   kit: {
     adapter: adapter(),
     paths: {
-      base: '/alex-nevsky'
-    }
+      // Для локалки оставляем пустым, для билда ставим подпапку
+      base: dev ? '' : '/alex-nevsky'
+    },
+    // Генерация относительных путей для всех ассетов
+    appDir: '_app'
   }
 };
 
